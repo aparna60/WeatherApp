2d4b4bb409421c65a5c90e51607e4ed61b59c288
aparna60 <abhattacharya60@gmail.com>
2025-08-14 13:12:37 +0530

    feat: Implement weather fetching functionality and UI enhancements
    - Added WeatherService to fetch weather data from an API.
    - Created IWeather interface to define the structure of weather data.
    - Updated AppComponent to handle weather data retrieval and display.
    - Integrated Bootstrap for improved styling and layout.
    - Enhanced global styles for a better user experience.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherService } from './services/weather.service';
+import { CommonModule } from '@angular/common';
+import { IWeather } from './services/iweather';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WeatherApp';
+  weatherData? : IWeather ;
+  city : string = 'Mumbai';
+  iconCode : string = '50d'
+  errorMessage: string = '';
+
+  service = inject(WeatherService);
+  
+  ngOnInit(): void {
+    this.getWeatherData(this.city);
+  }
+
+  getWeatherData(cityName : string)
+  {
+    this.service.getWeather(cityName).subscribe(
+       {
+       next: response => this.weatherData =response,    
+       error: (error)=> {this.errorMessage = error.error.message
+       console.log(error)}
+       }
+      )
+    
+  }
+
+  getCity(ev : Event)
+  {
+    const item= ev.target as HTMLInputElement
+    this.city= item.value.toLocaleLowerCase();
+    this.getWeatherData(this.city);
+
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,11 +1,36 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject} from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { IWeather } from './iweather';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
-  private http= inject(HttpClient);
+  private http = inject(HttpClient);
 
-  constructor() { }
+  constructor() {} 
+
+  // getWeather(city? : string) : Observable<IWeather> {
+  //   return this.http.get<IWeather>('https://weather-api99.p.rapidapi.com/weather?city='+ city, {
+  //     headers: {
+  //       'x-rapidapi-key': '0c669dbd7emsh58ed95c67fcbc8fp1f3448jsn1248e3c61bdd',
+  //       'x-rapidapi-host': 'weather-api99.p.rapidapi.com',
+  //     },
+  //   }).pipe(
+  //     catchError((err)=>{return throwError(()=> err)})
+  //   );
+  // }
+
+
+   getWeather(city? : string) : Observable<IWeather> {
+    return this.http.get<IWeather>('https://localhost:7061/api/Weather/?city='+ city, {
+      headers: {
+        'x-rapidapi-key': '0c669dbd7emsh58ed95c67fcbc8fp1f3448jsn1248e3c61bdd',
+        'x-rapidapi-host': 'weather-api99.p.rapidapi.com',
+      },
+    }).pipe(
+      catchError((err)=>{return throwError(()=> err)})
+    );
+  }
 }
